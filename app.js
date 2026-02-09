cc9ad3e38381d101cc1a2dfd72c7e75dc6d39c0c
Sinda <katfaouisinda9@gmail.com>
2026-02-10 00:27:19 +0100

    config server

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const http = require('http');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,7 +34,12 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json('error');
 });
 
-module.exports = app;
+//2
+const server = http.createServer(app);
+
+server.listen(5000,() =>{
+  console.log('server is running on http://localhost:5000');
+});
